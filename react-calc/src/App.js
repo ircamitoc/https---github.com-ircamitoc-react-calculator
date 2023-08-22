883286b4cf9cba0c84c9458c5a501a7788207846
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 00:14:04 +0800

    Fixed format

--- react-calc/src/App.js
+++ react-calc/src/App.js
@@ -1,33 +1,37 @@
-import { useReducer } from "react"
-import "./App.css"
+import { useReducer } from "react";
+import "./App.css";
 
 export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate'
-}
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
 
-function reducer (state, {type, payload}) {
-  switch(type) {
+function reducer(state, { type, payload }) {
+  switch (type) {
     case ACTIONS.ADD_DIGIT:
       return {
         ...state,
-        currentOperand: '${currentOperand || ""}${payload.digit}'
-      }
+        currentOperand: '${currentOperand || ""}${payload.digit}',
+      };
   }
 }
 
-function App () {
-
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
-  dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
+function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
 
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
@@ -49,7 +53,7 @@ function App () {
       <button>0</button>
       <button className="span-two">=</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
