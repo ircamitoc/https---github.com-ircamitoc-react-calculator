932d9a5a966a069158d259cd99ccfb6738a68087
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 00:16:28 +0800

    Fixed

--- react-calc/src/DigitButton.js
+++ react-calc/src/DigitButton.js
@@ -1,6 +1,6 @@
 import { ACTIONS } from "./App";
 
-export default function DigitButon({ dispatch, digit }) {
+export default function DigitButton({ dispatch, digit }) {
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
